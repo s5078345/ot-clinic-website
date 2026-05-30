ab71cc1518027b4d888239c7a626cdd36cd427f2
Sam <sam@streamlinebio.ai>
2026-05-30 13:34:01 +0800

    feat: make google can search this website

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/ot-clinic-website/",
   build: {
     rollupOptions: {
       input: {
